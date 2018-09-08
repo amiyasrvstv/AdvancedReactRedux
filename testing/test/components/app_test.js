cd9ae8599412f9b99e036f29e1604e86f49aeb7b
Amiya <amiyasrvstv@gmail.com>
2018-09-08 13:41:56 +0530

    adding CommentBox to App

--- testing/test/components/app_test.js
+++ testing/test/components/app_test.js
@@ -3,16 +3,15 @@ import App from '../../src/components/app';
 
 // Use describe to group together similar tests
 describe('App', () => {
-  // Use 'it' to test a single attribute of a target
-  it('shows the correct text', () => {
+  let component;
 
-    // Create and instance of App
-    const component = renderComponent(App);
-
-    // Use 'expect' to make an 'assertion' about a target
-    expect(component).to.contain('React simple starter');
-    
+  beforeEach(() => {
+    component = renderComponent(App);
   });
 
+  it('shows a comment box', () => {
+    expect(component.find('.comment-box')).to.exist;
+  });
+ 
 });
 
